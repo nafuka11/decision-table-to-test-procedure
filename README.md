b327c9f93a5dd47071eb180ae53a7515d89549a2
nafuka <42476527+nafuka11@users.noreply.github.com>
2018-12-15 23:31:13 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# decision-table-to-test-procedure
+
+## これは何？
+
+Excelのデシジョンテーブルを読込み、  
+標準出力にテスト手順を出力するスクリプトです。
---
+++ read_excel.py
@@ -0,0 +1,75 @@
+import xlrd
+
+# Excelファイルパス
+FILE_PATH = "decision_table.xlsx"
+# Excelシート名
+SHEET_NAME = "デシジョンテーブル"
+
+# デシジョンテーブル開始列数
+TABLE_START_INDEX = 1
+# デシジョンテーブル内での条件/結果開始行数
+CONDITION_COL_INDEX = 2
+# デシジョンテーブル内でのテスト項目開始列数
+TEST_COL_INDEX = 5
+
+
+def main():
+    # Excelを読込み、シートを取得
+    book = xlrd.open_workbook(filename=FILE_PATH)
+    sheet = book.sheet_by_name(SHEET_NAME)
+
+    # テスト項目が存在する行のみ取得
+    rows = [sheet.row_values(y) for y in range(sheet.nrows)]
+    rows = [x[TABLE_START_INDEX:] for x in rows
+            if x[TEST_COL_INDEX + TABLE_START_INDEX:]]
+
+    # 条件～次の条件の1つ前の行でテーブルを区切る
+    tests = []
+    start_index = 0
+    for i in range(len(rows)):
+        if rows[i][0] != "条件" or start_index == i:
+            continue
+        tests.append(rows[start_index:i])
+        start_index = i
+    tests.append(rows[start_index:])
+
+    # 標準出力にテスト手順を出力
+    for test in tests:
+        print_test_procedure(test)
+
+
+def print_test_procedure(rows):
+    """標準出力にテスト手順を出力"""
+    # セル結合により空文字となっている条件を補間する
+    for y in range(0, len(rows)):
+        for x in range(CONDITION_COL_INDEX, TEST_COL_INDEX - TABLE_START_INDEX + 1):
+            if not rows[y][x] and (x == CONDITION_COL_INDEX or
+                                   rows[max(y - 1, 0)]
+                                   [max(x - 1, CONDITION_COL_INDEX)] == rows[y][max(x - 1, CONDITION_COL_INDEX)]):
+                rows[y][x] = rows[max(y - 1, 0)][x]
+
+    # テスト項目毎に
+    for test_no in range(TEST_COL_INDEX - TABLE_START_INDEX + 1, len(rows[0])):
+        if not rows[0][test_no]:
+            break
+        print("----------------------------------------")
+        # デシジョンテーブルの行毎にテスト手順を標準出力する
+        for row in rows:
+            # 条件/結果
+            if row[0]:
+                print(f"{row[0]}：")
+            # テスト対象
+            if row[1]:
+                print(f"　[{row[1]}]")
+            # テスト内容
+            if row[test_no] == "-":
+                continue
+            print(f"　　{row[test_no]}: {row[CONDITION_COL_INDEX]}", end="")
+            for cond_index in range(CONDITION_COL_INDEX + 1, TEST_COL_INDEX):
+                if row[cond_index]:
+                    print(f": {row[cond_index]}", end="")
+            print()
+
+
+if __name__ == "__main__":
+    main()
