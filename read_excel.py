73719434ad72af5b07618223f6af6b856e1fcd77
nafuka <42476527+nafuka11@users.noreply.github.com>
2019-01-28 23:03:14 +0900

    fix: デシジョンテーブルのindex指定等修正

--- read_excel.py
+++ read_excel.py
@@ -8,7 +8,7 @@ SHEET_NAME = "デシジョンテーブル"
 # デシジョンテーブル開始列数
 TABLE_START_INDEX = 1
 # デシジョンテーブル内での条件/結果開始行数
-CONDITION_COL_INDEX = 2
+CONDITION_COL_INDEX = 3
 # デシジョンテーブル内でのテスト項目開始列数
 TEST_COL_INDEX = 5
 
@@ -21,7 +21,7 @@ def main():
     # テスト項目が存在する行のみ取得
     rows = [sheet.row_values(y) for y in range(sheet.nrows)]
     rows = [x[TABLE_START_INDEX:] for x in rows
-            if x[TEST_COL_INDEX + TABLE_START_INDEX:]]
+            if x[TEST_COL_INDEX + TABLE_START_INDEX] in ["Y", "N", "-"]]
 
     # 条件～次の条件の1つ前の行でテーブルを区切る
     tests = []
@@ -42,14 +42,12 @@ def print_test_procedure(rows):
     """標準出力にテスト手順を出力"""
     # セル結合により空文字となっている条件を補間する
     for y in range(0, len(rows)):
-        for x in range(CONDITION_COL_INDEX, TEST_COL_INDEX - TABLE_START_INDEX + 1):
-            if not rows[y][x] and (x == CONDITION_COL_INDEX or
-                                   rows[max(y - 1, 0)]
-                                   [max(x - 1, CONDITION_COL_INDEX)] == rows[y][max(x - 1, CONDITION_COL_INDEX)]):
-                rows[y][x] = rows[max(y - 1, 0)][x]
+        for x in range(2, TEST_COL_INDEX):
+            if not rows[y][x] and y != 0 and rows[y - 1]:
+                rows[y][x] = rows[y - 1][x]
 
     # テスト項目毎に
-    for test_no in range(TEST_COL_INDEX - TABLE_START_INDEX + 1, len(rows[0])):
+    for test_no in range(TEST_COL_INDEX, len(rows[0])):
         if not rows[0][test_no]:
             break
         print("----------------------------------------")
@@ -64,8 +62,8 @@ def print_test_procedure(rows):
             # テスト内容
             if row[test_no] == "-":
                 continue
-            print(f"　　{row[test_no]}: {row[CONDITION_COL_INDEX]}", end="")
-            for cond_index in range(CONDITION_COL_INDEX + 1, TEST_COL_INDEX):
+            print(f"　　{row[test_no]}: {row[2]}", end="")
+            for cond_index in range(3, TEST_COL_INDEX):
                 if row[cond_index]:
                     print(f": {row[cond_index]}", end="")
             print()
